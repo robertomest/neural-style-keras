35e28763f6ba37f4137959b907639c6bc85b56da
Roberto Estevão <robertomest@poli.ufrj.br>
2017-02-09 14:52:43 -0200

    Fix imports on utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,14 @@
 '''
 Module constains utilitary functions.
 '''
+import tensorflow as tf
+from keras.preprocessing.image import load_img, img_to_array
+import numpy as np
+import h5py
+import yaml
+from PIL import Image
+from keras.applications import vgg16
+from keras import backend as K
 
 def config_gpu(gpu, allow_growth):
     # Choosing gpu
