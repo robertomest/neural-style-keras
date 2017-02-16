ac7edef7cc79e35a9e28cbb204302a02cfc1628f
Roberto Estevão <robertomest@poli.ufrj.br>
2017-02-16 09:55:06 -0200

    Added multi-style models to download script

--- data/models/download_models.sh
+++ data/models/download_models.sh
@@ -3,3 +3,5 @@ wget -c -O feathers.h5 https://www.dropbox.com/s/im79c6ni2a4hl19/feathers.h5?dl=
 wget -c -O muse.h5 https://www.dropbox.com/s/2d45czsn07arfpa/muse.h5?dl=0
 wget -c -O scream.h5 https://www.dropbox.com/s/ku8r2h1nz1h8kow/scream.h5?dl=0
 wget -c -O udnie.h5 https://www.dropbox.com/s/bmwxg6ny9dg80dm/udnie.h5?dl=0
+wget -c -O candy_feathers.h5 https://www.dropbox.com/s/5bjwqgkrumsv20w/candy_feathers.h5?dl=0
+wget -c -O 6_styles.h5 https://www.dropbox.com/s/6hwusy6qifcwg8r/6_styles.h5?dl=0
